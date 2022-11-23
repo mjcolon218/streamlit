327968ff7edefd0c87d8f6ae55eef33ff7cf4898
MauriceJColon <mjcolon218@gmail.com>
2022-11-23 16:45:21 -0500

    Hey moe

---
+++ analytics.py
@@ -0,0 +1,19 @@
+import yfinance as yf
+import streamlit as st
+import pandas as pd
+
+st.write("""
+# Simple Stock Price App
+
+Shown are the stock closing price and Volume of Google!
+
+""")
+tickerSymbol = "GOOGL"
+tickerData = yf.Ticker(tickerSymbol)
+
+tickerDf =tickerData.history(period='1d', start='2019-1-01', end='2022-1-01')
+print(type(tickerDf))
+print(tickerDf.info())
+st.line_chart(tickerDf.Close)
+st.line_chart(tickerDf.Volume)
+
